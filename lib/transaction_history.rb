07eb84e14d4f2d7fff0f735307b4db49f0fb9a99
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-06-18 17:18:58 +0100

    created transaction history class to log transactions

--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -1,2 +1,8 @@
 class TransactionHistory
+
+  attr_reader :log
+
+  def initialize
+    @log = []
+  end
 end
--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -2,9 +2,9 @@ require 'transaction_history'
 
 describe TransactionHistory do
   let(:transaction_history) { TransactionHistory.new }
-  describe '#log' do
-    it 'should be a hash' do
-      expect(transaction_history.log).to eq {}
+  describe '#initialize' do
+    it 'should initialize with an empty log array' do
+      expect(transaction_history.log).to eq []
     end
   end
 end
