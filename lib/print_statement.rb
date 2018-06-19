713c93f8ba78cce4f03cdfaa7682d5243a53135f
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-06-19 16:34:57 +0100

    print statement prints transaction_log to console

---
+++ lib/print_statement.rb
@@ -0,0 +1,19 @@
+class PrintStatement
+
+  attr_reader :transaction_log
+
+  def initialize(transaction_log)
+    @statement = transaction_log
+  end
+
+  def print_statement
+    spacing = 10
+      print "Date    || Credit || Debit || balance\n"
+    @statement.each do |transaction|
+      print "#{transaction[:date]} "
+      print  "|| #{transaction[:credit]} "
+      print "|| #{transaction[:debit]} "
+      print "|| #{transaction[:balance]}\n"
+    end
+  end
+end
--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -1,10 +1,13 @@
 require 'print_statement'
 
 describe PrintStatement do
-  let(:pint_statement) { PrintStatement.new(statment)}
-  let(:transaction_log) { { :debit => " ", :credit => amount, :date => date, :balance => balance } }
+  let(:transaction_log) { [{ :debit => " ", :credit => 2000, :date => "19-06-2018", :balance => 4000 }] }
+  let(:printstatement) { PrintStatement.new(transaction_log)}
 
-  it 'should receive the transaction_log on #initialize' do
-    expect(PrintStatement.new).to_receive (:transaction_log)
+  describe '#print_statement' do
+    it 'should print the transaction_log to console' do
+    expect { printstatement.print_statement }.to output("Date    || Credit || Debit || balance\n19-06-2018 || 2000 ||   || 4000\n").to_stdout
+    end
   end
+  
 end
