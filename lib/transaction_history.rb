fba917a7f0f6b84df1ff42f787ed1af3ed8e6276
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-06-19 09:36:18 +0100

    failing test for adding transaction history to log

--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -1,8 +1,15 @@
+require 'bank_account'
+
 class TransactionHistory
 
-  attr_reader :log
+  attr_reader :transaction_log, :date, :balance, :credit, :debit
 
   def initialize
-    @log = []
+    @transaction_log = []
   end
+
+  def add_to_log
+  end
+
+
 end
--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -2,9 +2,18 @@ require 'transaction_history'
 
 describe TransactionHistory do
   let(:transaction_history) { TransactionHistory.new }
+  let(:new_account) { BankAccount.new }
+
   describe '#initialize' do
-    it 'should initialize with an empty log array' do
-      expect(transaction_history.log).to eq []
+    it 'should initialise with an empty #transaction_log array' do
+      expect(transaction_history.transaction_log).to be_empty
+    end
+  end
+
+  describe '#add_to_log' do
+    it 'should add transaction amount when money is deposited' do
+      new_account.deposit(2000)
+      expect(transaction_history.transaction_log).to include(:balance)
     end
   end
 end
