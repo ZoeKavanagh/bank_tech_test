4076b382fbde0e9dda9935cebb46383a240e522a
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-06-19 12:29:45 +0100

    add debit method test now passing

--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -10,5 +10,9 @@ class TransactionHistory
     @transaction_log << { :debit => " ", :credit => amount, :date => date, :balance => balance }
   end
 
+  def add_debit(amount, date=DateTime.now.strftime("%d-%m-%Y"), balance)
+    @transaction_log << { :debit => amount, :credit => " ", :date => date, :balance => balance }
+  end
+
 
 end
