6078382054bc35bc05d45ce0fb83c70d4a1fab00
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-06-19 16:38:19 +0100

    first failing test for bank account print bank satement method

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,6 +4,8 @@ require 'transaction_history'
 describe BankAccount do
   let(:new_account) { BankAccount.new(transaction_history) }
   let(:transaction_history) { spy :transaction_history }
+  let(:transaction_log) { [{ :debit => " ", :credit => 2000, :date => "19-06-2018", :balance => 4000 }] }
+  let(:printstatement) { spy :print_statement }
 
   it { is_expected.to respond_to :balance }
 
@@ -39,7 +41,9 @@ describe BankAccount do
     end
   end
 
-  describe '#print_statement' do
-    it 'should '
+  describe '#print_bank_statement' do
+    it 'should take the transaction_log and print to the console' do
+      expect { new_account.print_bank_statement(:transaction_log) }.to output("Date    || Credit || Debit || balance\n19-06-2018 || 2000 ||   || 4000\n").to_stdout
+    end
   end
 end
